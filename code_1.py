bff466f5babf3efeff555be7f3c601eea7f7d813
caltvos <152297554+caltvos@users.noreply.github.com>
2023-12-22 17:37:54 +0000

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,52 +1,26 @@
 
     for item in data:
+def generate_random_data():
 
-
+    return data
+    main()
+def main():
     data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    return data
-def main():
-    data = generate_random_data()
-    main()
-def generate_random_data():def main():
-def generate_random_data():
-    for item in data:
-
-    main()
-    data = generate_random_data()
-if __name__ == "__main__":
-
-
-    return data
-
-import random
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]    return data
 import random
 
-
-    for item in data:
-
-def generate_random_data():
-    main()
+        print(f"Random Number: {item}")
 if __name__ == "__main__":
     data = generate_random_data()
-def main():
-        print(f"Random Number: {item}")
-
-    data = [random.randint(1, 100) for _ in range(10)]        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-    }
-}
-    public static void main(String[] args) {
             data[i] = (int) (Math.random() * 100) + 1;
-
-
         int[] data = new int[10];
+
+    public static void main(String[] args) {
         }
-            System.out.println("Random Number: " + item);
 public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
         }
+
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+}
+    }
